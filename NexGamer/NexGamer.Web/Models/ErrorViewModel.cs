f89066fea5b516c89fbc84783e8c2cc70bc89c31
Cooper Bodman <cooperjack23@gmail.com>
2024-04-07 21:01:49 -0700

    Added New MVC Project

---
+++ NexGamer/NexGamer.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NexGamer.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
